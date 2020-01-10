e86b5be56f6246535ce6a88411a8f57a61fdcca4
Leonid Efimov <efimleonid@yandex.ru>
2020-01-10 22:34:50 +0300

    Write the read by user text

--- file2.c
+++ file2.c
@@ -1,8 +1,11 @@
 int read(int fd, char *buf, int n);
 int write(int fd, char *buf, int n);
+#define BUFSIZE 1024
 
 int main(){
- char *buf = "trying\n";
- write(1, buf, sizeof(buf));
+ char buf[BUFSIZE];
+ int n, k;
+ n = read(0, buf, BUFSIZE);
+ write(1, buf, n);
  return 0;
 }
